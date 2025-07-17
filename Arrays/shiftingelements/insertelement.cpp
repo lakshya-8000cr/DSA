6e14737e1e8312d711eb7af70bd476fc91b96beb
lakshya-8000cr <lakshyachauhan147@gmail.com>
2025-07-17 20:34:40 +0530

    added the inserting of an element

---
+++ Arrays/shiftingelements/insertelement.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std ;
+
+int main(){
+    int n = 4;
+    int arr[100] = {1,2,3,4};
+
+    int k = 1;
+    int x = 99;
+
+    for(int i=n-1 ; i>=k ; i--){
+        arr[i+1] = arr[i];
+    }
+
+    arr[k] = x;
+    n++;
+
+    for(int i=0;i<n; i++){
+        cout<<arr[i];
+    }
+}
