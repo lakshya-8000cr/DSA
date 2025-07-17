0c95fc305c928fb54a7d679a9c09781dfd537bf9
lakshya-8000cr <lakshyachauhan147@gmail.com>
2025-07-17 21:26:43 +0530

    added the movezero to end element

--- Arrays/shiftingelements/movenegativetofront.cpp
+++ Arrays/shiftingelements/movenegativetofront.cpp
@@ -1,23 +1,26 @@
 #include<iostream>
-using namespace std ;
+using namespace std;
 
-int main(){
+int main() {
     int n = 5;
-    int arr[n]={1,-2,3,-4,5};
+    int arr[n] = {1, -2, 3, -4, 5};
 
-    int j = 0;
-    int i = 0;
-    int k = n-1;
-    while(j<n){
-        if(arr[n]<0){
-            arr[j++] = arr[k];
-            k--;
+    int j = 0; 
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < 0) {
+            if (i != j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+            j++; 
         }
     }
-    
 
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<",";
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
     }
 
     return 0;
