4f6649b4d4475c16abf25c67445308ced5e39cd6
lakshya-8000cr <lakshyachauhan147@gmail.com>
2025-09-02 18:23:40 +0530

    added the

---
+++ REVISION/SORTING/selectionsort.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+   int n ;
+   cin>>n;
+   vector<int>arr = {10 , 9 , 20 , 6 , 1  };
+   int n = arr.size();
+   for(int i=0; i<n-1; i++){
+       int firstindex = i;
+       for(int j = i; j<n; j++){
+           
+       }
+   }
+}
