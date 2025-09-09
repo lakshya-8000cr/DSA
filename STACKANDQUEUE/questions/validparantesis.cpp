4535fbaab8288ec90229f2750e9fe057386f3907
lakshya-8000cr <lakshyachauhan147@gmail.com>
2025-09-09 23:01:46 +0530

    added the stack and queue

---
+++ STACKANDQUEUE/questions/validparantesis.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class valid {
+public:
+    bool validparenthesis(string s) {
+        stack<char> St;  
+        int len = s.length();
+
+        for (int i = 0; i < len; i++) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                St.push(s[i]);
+            }
+            else {
+                if (St.empty()) return false; 
+
+                char ch = St.top();
+                St.pop();
+
+                if ((ch == '(' && s[i] != ')') ||
+                    (ch == '[' && s[i] != ']') ||
+                    (ch == '{' && s[i] != '}')) {
+                    return false; 
+                }
+            }
+        }
+
+        return St.empty(); 
+    }
+};
+
+int main() {
+    valid V;
+    string s;
+    getline(cin, s);
+    cout << (V.validparenthesis(s) ? "Valid" : "Invalid");
+}
